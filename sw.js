7514a5a61a441c2f79c65ed9597175cdff62968b
e-massage-app <gamezoneyt@gmail.com>
2026-04-02 15:04:49 +0200

    Depenses: filters all on 1 row (flex nowrap). SW v10

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // Strategie : cache assets statiques UNIQUEMENT, PAS les donnees Supabase
 // Si offline -> l'app affiche le message "reseau requis"
 
-const CACHE_NAME = 'elise-massage-v9';
+const CACHE_NAME = 'elise-massage-v10';
 const STATIC_ASSETS = [
   './',
   './index.html',
